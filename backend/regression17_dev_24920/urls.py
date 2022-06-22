db0e8ded37490f630c35124888d7804d264f0923
Crowdbotics <engineering@crowdbotics.com>
2022-06-22 10:38:49 +0000

    Update admin title

--- backend/regression17_dev_24920/urls.py
+++ backend/regression17_dev_24920/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Regression17s"
-admin.site.site_title = "Regression17s Admin Portal"
-admin.site.index_title = "Regression17s Admin"
+admin.site.site_header = "rgression17s"
+admin.site.site_title = "rgression17s Admin Portal"
+admin.site.index_title = "rgression17s Admin"
 
 # swagger
 api_info = openapi.Info(
